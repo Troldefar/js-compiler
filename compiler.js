b3783f28f5d6c8066320bcbbef0b97f8c9b8fb65
troldefar <r_emil@live.dk>
2023-08-17 21:23:50 +0200

    js compiler... because why not

---
+++ compiler.js
@@ -0,0 +1,26 @@
+module.exports = function compiler(input) {
+    /**
+     * Step 1
+     *  - Lexical analysis
+    */
+
+    /**
+     * Step 2
+     *  - Syntactic analysis
+    */
+
+    /**
+     * Step 3
+     *  - Transformation
+    */
+
+    /**
+     * Step 4
+     *  - Code generation
+    */
+
+    /**
+     * Step 5
+     *  - return
+    */
+}
