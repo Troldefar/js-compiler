a5d13299e6757bac7b976d89f505f4db87955d71
troldefar <r_emil@live.dk>
2023-08-17 21:38:32 +0200

    init

--- tokenizer.js
+++ tokenizer.js
@@ -1,3 +1,9 @@
 module.exports = function tokenizer(input) {
-    
+    const tokens = [];
+    let current = 0;
+    while ( current < input.length ) {
+        const char = input[current];
+        throw new TypeError(`Unknowon character was encountered ${char}`);
+    }
+    return tokens;
 }
