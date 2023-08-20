a5273c13a6580444bf09e9cce3599e9a2651760e
troldefar <r_emil@live.dk>
2023-08-20 10:14:39 +0200

    rfc

--- parser.js
+++ parser.js
@@ -1,30 +1,29 @@
 
 module.exports = function parser(tokens) {
     let current = 0;
-    
+    console.log("Iterating: \n", tokens);
     function walk() {
-        let token = tokens[current];
-        if (token.type === 'number') {
-            current++;
-            return {type: 'NumberLiteral', value: token.value};
-        }
-        if (token.type === 'paren' && token.value === '(') {
-            token = tokens[++current];
-            const expression = {
-                type: 'CallExpression',
-                name: token.value,
-                params: []
-            };
-            token = tokens[++current];
-            while (token.value !== ')') {
-                expression.params.push(walk());
-                token = tokens[current];
+        try {
+            let token = tokens[current];
+            console.log("Current: ", token);
+            if (token.type === 'number') {
+                current++;
+                return {type: 'NumberLiteral', value: token.value};
             }
-            current++;
-            return expression;
+            if (token.type === 'paren' && token.value === '(') {
+                token = tokens[++current];
+                const expression = {type: 'CallExpression', name: token.value, params: []};
+                token = tokens[++current];
+                while (token.value !== ')') {
+                    expression.params.push(walk());
+                    token = tokens[current];
+                }
+                current++;
+                return expression;
+            }
+        } catch(e) {
+            console.log(e);
         }
-
-        throw new TypeError(`Unknown token: '${token}'`);
     }
 
     const tree = {type: 'Program', body: [walk()]};
