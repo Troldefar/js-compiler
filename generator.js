52aac63a0ffe13199ebc441da49ea13313460796
troldefar <r_emil@live.dk>
2023-08-21 06:37:21 +0200

    static test

--- generator.js
+++ generator.js
@@ -1,8 +1,7 @@
 module.exports = function generator(node) {
-    
     if (node.type === 'NumericLiteral') return node.value;
     if (node.type === 'Identifier') return node.name;
     if (node.type === 'CallExpression') return `${generator(node.callee)}(${node.arguments.map(generator).join(', ')})`;
     if (node.type === 'ExpressionStatement') return `${generator(node.expression)};`;
     if (node.type === 'Program') return node.body.map(generator).join('\n');
-  }
+}
