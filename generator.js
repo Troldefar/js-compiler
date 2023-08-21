af20bfd536e6dceccbf9a834001e814ed8373056
troldefar <r_emil@live.dk>
2023-08-21 06:34:00 +0200

    minor generator

--- generator.js
+++ generator.js
@@ -1,8 +1,8 @@
-module.exports = function generateCode(node) {
+module.exports = function generator(node) {
     
     if (node.type === 'NumericLiteral') return node.value;
     if (node.type === 'Identifier') return node.name;
-    if (node.type === 'CallExpression') return `${generateCode(node.callee)}(${node.arguments.map(generateCode).join(', ')})`;
-    if (node.type === 'ExpressionStatement') return `${generateCode(node.expression)};`;
-    if (node.type === 'Program') return node.body.map(generateCode).join('\n');
+    if (node.type === 'CallExpression') return `${generator(node.callee)}(${node.arguments.map(generator).join(', ')})`;
+    if (node.type === 'ExpressionStatement') return `${generator(node.expression)};`;
+    if (node.type === 'Program') return node.body.map(generator).join('\n');
   }
