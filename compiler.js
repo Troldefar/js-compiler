3a11eccb77f53d8701207c54980a1dafe7280b6e
Troldefar <r_emil@live.dk>
2023-09-14 06:29:58 +0200

    Update compiler.js

--- compiler.js
+++ compiler.js
@@ -4,40 +4,9 @@ const transformer = require('./transformer');
 const generator = require('./generator');
 
 module.exports = function compiler(input) {
-    /**
-     * Step 1
-     *  - Lexical analysis
-     *  - Collect syntax pieces
-    */
-
     const tokens = tokenizer(input);
-
-    /**
-     * Step 2
-     *  - Syntactic analysis
-     *  - Create AST
-    */
-
     const syntaxTree = parser(tokens);
-
-    /**
-     * Step 3
-     *  - Transformation
-    */
-
     const transformed = transformer(syntaxTree);
-
-    /**
-     * Step 4
-     *  - Code generation
-    */
-
     const generatedCode = generator(transformed);
-
-    /**
-     * Step 5
-     *  - return
-    */
-
     return generatedCode;
 }
